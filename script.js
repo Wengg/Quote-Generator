263226700a6e0efe31e4c2956545cd58da07b610
Karen Weng Liang <weng@Karens-MacBook-Pro.local>
2020-08-17 16:10:19 -0500

    update code

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
-console.log('hello world');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function showLoading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoading() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+async function getQuoteAPI() {
+
+    showLoading();
+    const proxyUrl = 'https://polar-tundra-14135.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+
+        // If author is blank, add 'Unknown'
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+
+        // Reduce font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+
+        quoteText.innerText = data.quoteText;
+        removeLoading();
+
+    } catch (error) {
+        getQuoteAPI();
+    }
+}
+
+// Tweet Quote 
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = quoteText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuoteAPI);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+getQuoteAPI();
